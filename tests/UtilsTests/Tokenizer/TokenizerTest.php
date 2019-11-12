154e6722a864e5018fef881eeabb326ad3ad024c
Jakub Filla <jakub.filla@wavevision.cz>
2019-11-12 13:25:29 +0100

    Normalize tokenizer test

--- tests/UtilsTests/Tokenizer/TokenizerTest.php
+++ tests/UtilsTests/Tokenizer/TokenizerTest.php
@@ -1,6 +1,6 @@
 <?php declare (strict_types = 1);
 
-namespace Wavevision\UtilsTests;
+namespace Wavevision\UtilsTests\Tokenizer;
 
 use org\bovigo\vfs\vfsStream as fs;
 use PHPUnit\Framework\TestCase;
