21613e5885e733b58115e40437169a49ab2a48e4
Jakub Filla <jakub.filla@wavevision.cz>
2019-11-14 15:45:01 +0100

    Add image info

---
+++ src/Utils/ImageInfo.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types = 1);
+
+namespace Wavevision\Utils;
+
+use Nette\InvalidStateException;
+
+class ImageInfo
+{
+
+	/**
+	 * @var int
+	 */
+	private $width;
+
+	/**
+	 * @var int
+	 */
+	private $height;
+
+	/**
+	 * @var string
+	 */
+	private $contentType;
+
+	public static function create(string $pathname): self
+	{
+		$info = getimagesize($pathname);
+		if ($info === false) {
+			throw new InvalidStateException("Filepath '$pathname' is not valid image.");
+		}
+		return new self($info[0], $info[1], $info['mime']);
+	}
+
+	public function getContentType(): string
+	{
+		return $this->contentType;
+	}
+
+	public function getWidth(): int
+	{
+		return $this->width;
+	}
+
+	public function getHeight(): int
+	{
+		return $this->height;
+	}
+
+	private function __construct(int $width, int $height, string $contentType)
+	{
+		$this->width = $width;
+		$this->height = $height;
+		$this->contentType = $contentType;
+	}
+
+}
---
+++ tests/UtilsTests/ImageInfoTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Wavevision\UtilsTests;
+
+use Nette\InvalidStateException;
+use PHPUnit\Framework\TestCase;
+use Wavevision\Utils\ImageInfo;
+use Wavevision\Utils\Path;
+
+class ImageInfoTest extends TestCase
+{
+
+	public function testCreateInvalid(): void
+	{
+		$this->expectException(InvalidStateException::class);
+		$this->expectExceptionMessage(sprintf("Filepath '%s' is not valid image.", __FILE__));
+		ImageInfo::create(__FILE__);
+	}
+
+	public function testCreateValid(): void
+	{
+		$info = ImageInfo::create(Path::join(__DIR__, 'files', 'image.png'));
+		$this->assertEquals(921, $info->getWidth());
+		$this->assertEquals(589, $info->getHeight());
+		$this->assertEquals('image/png', $info->getContentType());
+	}
+
+}
