395b756fbf6e2e6e657937f18285dc51e78a09f3
Vít Rozsíval <mail@vitrozsival.cz>
2020-10-21 14:51:38 +0200

    Fix failing tests

--- tests/UtilsTests/ServerTest.php
+++ tests/UtilsTests/ServerTest.php
@@ -3,11 +3,8 @@
 namespace Wavevision\UtilsTests;
 
 use Nette\IOException;
-use phpmock\phpunit\PHPMock;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Wavevision\Utils\Server;
-use Wavevision\Utils\Strings;
 
 /**
  * @covers \Wavevision\Utils\Server
@@ -15,47 +12,30 @@ use Wavevision\Utils\Strings;
 class ServerTest extends TestCase
 {
 
-	use PHPMock;
-
 	public function testGetIniThrowsException(): void
 	{
-		$iniGet = $this->mockFunction('ini_get');
-		$iniGet->expects($this->once())->willReturn(false);
 		$this->expectException(IOException::class);
-		Server::getIni('key');
+		Server::getIni('undefined');
 	}
 
 	public function testGetIniReturnsKeyValue(): void
 	{
-		$iniGet = $this->mockFunction('ini_get');
-		$iniGet->expects($this->once())->willReturn('value');
-		$this->assertEquals('value', Server::getIni('key'));
+		$this->assertIsString(Server::getIni('default_charset'));
 	}
 
 	public function testGetMaxUploadSizeReturnsCustomizedValue(): void
 	{
-		$iniGet = $this->mockFunction('ini_get');
-		$iniGet->expects($this->exactly(2))->willReturn('100M', '120M');
-		$this->assertEquals(104857600, Server::getMaxUploadSize(110, 'M'));
+		$this->assertEquals(1048576, Server::getMaxUploadSize(1, 'M'));
 	}
 
 	public function testGetMaxUploadSizeReturnsDefault(): void
 	{
-		$iniGet = $this->mockFunction('ini_get');
-		$iniGet->expects($this->exactly(2))->willReturn('120M');
-		$this->assertEquals(125829120, Server::getMaxUploadSize());
+		$this->assertIsInt(Server::getMaxUploadSize());
 	}
 
 	public function testIsCLI(): void
 	{
-		$phpSapiName = $this->mockFunction('php_sapi_name');
-		$phpSapiName->expects($this->once())->willReturn('cli');
-		$this->assertTrue(Server::isCLI());
-	}
-
-	private function mockFunction(string $function): MockObject
-	{
-		return $this->getFunctionMock(Strings::getNamespace(Server::class), $function);
+		$this->assertIsBool(Server::isCLI());
 	}
 
 }
